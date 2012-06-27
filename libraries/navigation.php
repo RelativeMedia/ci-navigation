7f799047acf8c9e8bfa879e655c69cd4b2649e3f
Mike DeVita <mdevita@fivepints.com>
2012-06-27 13:15:55 -0700

    made the prepend/append and nav_element dynamic, segregated some code to their own functions

--- libraries/navigation.php
+++ libraries/navigation.php
@@ -6,11 +6,8 @@ class Navigation {
 	/** @var array built navigation items */
 	protected static $_navs;
 
-	public static $nav_prepend = '<li>';
-	public static $nav_prepend_active_begin = '<li class="';
-	public static $nav_prepend_active_end = '">';
-	public static $nav_active_class = 'active';
-	public static $nav_append = '</li>';
+    public static $active_class = 'active';
+    public static $element_type = 'li';
 
     function __construct($params = array()) {
         self::$ci = & get_instance();
@@ -20,7 +17,7 @@ class Navigation {
      * parses the navigation from the view
      *
      * this function is typically called from a view, it is responsible for parsing navigation
-     * into a set of li/href elements. This function also determines the active link and 
+     * into a set of li/href elements. This function also determines the active link and
      * assigns a class to it.
      *
      * @param  string $url    string or array of urls, if array,other params are invalid
@@ -55,6 +52,57 @@ class Navigation {
     }
 
     /**
+     * [_prepend description]
+     * @param  boolean $active whether the navigation item is active or not
+     * @param  array   $params parameters to be passed
+     * @return string          returns back the prepended navigation element
+     */
+    private static function _prepend( $active = false, $params = array() ){
+        $prepend = '<'.self::$element_type;
+
+        /** did we pass any class names in? If so, implode the array into a string */
+        if ( isset($params['class']) ){
+            $classes = implode($params['class'], ' ');
+        }
+        /** Is the navigation item active? If so prepend the active_class object */
+        if ($active){
+            /** Since the nav element is active, add the active class */
+            $prepend .= ' class="'.self::$active_class;
+            /** Were other classes defined, if true then add them to the nav element */
+            if( isset($classes) ){
+                $prepend .= ' '.$classes.'"';
+            /** Otherwise close the class on the nav element */
+            }else{
+                $prepend .= '"';
+            }
+        /** Otherwise just use the passes classes if they're set. */
+        }else{
+            /** were other classes defined, if true then add them to the nav element */
+            if( isset($classes) ){
+                $prepend .= ' class="'.$classes.'"';
+            }
+        }
+        /** did we pass an id in? If so add it to the nav element*/
+        if( isset($params['id']) ){
+            $prepend .= ' id="'.$ids.'"';
+        }
+        /** Did we pass any data objects in? If so add them to the nav element */
+        if( isset($params['data']) ){
+
+        }
+
+        /** Close the element */
+        $prepend .= ' >';
+
+        return $prepend;
+    }
+
+    private static function _append( $params = array() ){
+        $append = '</'.self::$element_type.'>';
+        return $append;
+    }
+
+    /**
      * builds the navigation and returns it
      * @param  string $url    string or array of urls, if array,other params are invalid
      * @param  string $title  what to display in the href
@@ -64,10 +112,20 @@ class Navigation {
     private static function _build( $url, $title, $params = array() ){
         /** if the passed url is equal to the current uri_string, set this li active */
         if( $url == self::$ci->uri->uri_string() || $url == current_url() ){
-            $built = self::$nav_prepend_active_begin.self::$nav_active_class.self::$nav_prepend_active_end.anchor( $url, $title, $params ).self::$nav_append."\n";
+
+            $built =
+                    self::_prepend(true).
+                    anchor( $url, $title, $params ).
+                    self::_append()."\n";
+
         /** else if the passed url is equal to the base_url(), set this li active */
         }else{
-            $built = self::$nav_prepend.anchor( $url, $title, $params ).self::$nav_append."\n";
+
+            $built =
+                    self::_prepend().
+                    anchor( $url, $title, $params ).
+                    self::_append()."\n";
+
         }
         return $built;
     }
